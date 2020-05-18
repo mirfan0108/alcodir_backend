4fb07a5f964f6c78f0ece131e9ecce2324939c74
januarelsan <januarelsan@gmail.com>
2020-05-19 05:10:14 +0700

    Add Request Vpoint Validator

--- src/routes/management/Management.Route.js
+++ src/routes/management/Management.Route.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
-const { navigator } = require('../controller/Privilege.Ctrl')
-const management = require('../controller/management/vpoint.controller')
-const { isAuth } = require('./middleware/auth.js')
+const { navigator } = require('../../controller/Privilege.Ctrl')
+const management = require('../../controller/management/vpoint.controller')
+const validator = require('../middleware/validator.js')
+const { isAuth } = require('../middleware/auth.js')
+
 /** 
  * START POST
  */
 // TODO POST HERE
 // router.POST('/management/request/vpoint', isAuth, navigator, management.createVpointRequest)
-router.post('/request/vpoint',  management.createVpointRequest)
+router.post('/request/vpoint', validator.validate('requestVpoint') , management.createVpointRequest)
 router.post('/review/vpoint',  management.reviewVpointRequest)
 /** 
 * END POST
